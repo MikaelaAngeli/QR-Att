fd7c3a3299b36082cd2e91159b71f509a22fad4d
MikaelaAngeli <mikaelaangelibagata@gmail.com>
2026-09-17 15:58:32 +0800

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.headerContainer}>
-        <Header title="QR Attendance" />
+        <Header title="QR Attendance BAGATA" />
       </View>
 
       <View style={styles.bodyContainer}>
